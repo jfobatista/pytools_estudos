707042edcc693574641592d09fec6cdb51cc3f9c
João Francisco <jofrancb@users.noreply.github.com>
2022-02-19 21:36:05 -0300

    Alteração no nome do Pacote para upload no PyPI

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpytools"
+PACKAGE = "libpytools_jfobatista"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "João Francisco"
