fe2aefa2d7f989b235e6d11db32a82c690bdd177
João Francisco <jofrancb@users.noreply.github.com>
2022-02-22 19:26:36 -0300

    Produção de código testável, alterando código de produção (o que não é uma boa prática)

--- libpytools_jfobatista/testes/test_spam/test_usuarios.py
+++ libpytools_jfobatista/testes/test_spam/test_usuarios.py
@@ -2,13 +2,13 @@ from libpytools_jfobatista.testes.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='João')
+    usuario = Usuario(nome='João', email='jonesjoao_@ohotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='João'), Usuario(nome='Francisco')]
+    usuarios = [Usuario(nome='João', email='jonesjoao_@ohotmail.com'), Usuario(nome='Francisco', email='jonesjoao_@ohotmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
--- libpytools_jfobatista/testes/test_spam/teste_envio_para_base_de_usuarios.py
+++ libpytools_jfobatista/testes/test_spam/teste_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpytools_jfobatista.spam.main import EnviadorDeSpam
 from libpytools_jfobatista.testes.spam.enviador_de_email import Enviador
+from libpytools_jfobatista.testes.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='João', email='jonesjoao_@ohotmail.com'),
+            Usuario(nome='Francisco', email='jonesjo_@ohotmail.com')
+        ],
+        [
+            Usuario(nome='João', email='ja@ohotmail.com'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@python.pro.br',
         'Curso Python Pro',
         'Confira os módulos'
     )
+    assert len(usuarios) == enviador.qtd_emails_enviados
